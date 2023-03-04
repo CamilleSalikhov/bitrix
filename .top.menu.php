30f36d357196883c28f8baa1d260f961a83e3324
= <=>
2023-03-04 21:52:50 +0300

    first commit

---
+++ .top.menu.php
@@ -0,0 +1,46 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Главная страница", 
+		"/glavnaya-stranitsa/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Объявления", 
+		"/obyavleniya/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"/o-servise/novosti/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О сервисе", 
+		"/o-servise/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Личный кабинет продавца", 
+		"/lichnyy-kabinet-prodavtsa/", 
+		Array(), 
+		Array(), 
+		"CSite::InGroup(array(1,7))" 
+	),
+	Array(
+		"Личный кабинет покупателя", 
+		"/lichnyy-kabinet-pokupatelya/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
