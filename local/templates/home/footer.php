3f643711e25218fadd6e3405b8ecccbd355a7f88
= <=>
2023-03-07 21:33:33 +0300

    3.2 1try

--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -2,17 +2,45 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-4">
-          <div class="mb-5">
+          <!-- <div class="mb-5">
             <h3 class="footer-heading mb-4">About HomeSpace</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque,
               consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima
               minus odio!</p>
-          </div>
+          </div> -->
+          <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myabout.php"
+	)
+);?>
 
 
 
         </div>
         <div class="col-lg-4 mb-5 mb-lg-0">
+
+        <?$APPLICATION->IncludeComponent(
+	"bitrix:menu",
+	"",
+	Array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(""),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "N",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "N"
+	)
+);?>
+<!-- Компонент меню - верхнее меню, просто разместить -->
           <div class="row mb-5">
             <div class="col-md-12">
               <h3 class="footer-heading mb-4">Navigations</h3>
@@ -38,7 +66,7 @@
 
         </div>
 
-        <div class="col-lg-4 mb-5 mb-lg-0">
+        <!-- <div class="col-lg-4 mb-5 mb-lg-0">
           <h3 class="footer-heading mb-4">Follow Us</h3>
 
           <div>
@@ -50,19 +78,41 @@
 
 
 
-        </div>
+        </div> -->
+
+        <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myfollow.php"
+	)
+);?>
 
       </div>
       <div class="row pt-5 mt-5 text-center">
         <div class="col-md-12">
-          <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          <!-- <p>
+             
             Copyright &copy;</script>
             <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
             <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
               target="_blank">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-          </p>
+             
+          </p> -->
+
+          <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/mycopy.php"
+	)
+);?>
         </div>
 
       </div>
--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -80,17 +80,49 @@ IncludeTemplateLangFile(__FILE__);
         <div class="row align-items-center">
           <div class="col-6 col-md-6">
             <p class="mb-0">
-              <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+              <!-- <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
+                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a> -->
+                  <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/mynumber.php"
+	)
+);?>
+              
+                  <!-- <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
+                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a> -->
+                  <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myemail.php"
+	)
+);?>
             </p>
           </div>
-          <div class="col-6 col-md-6 text-right">
+          <!-- <div class="col-6 col-md-6 text-right">
             <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
             <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
             <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
-          </div>
+          </div> -->
+          <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myicons.php"
+	)
+);?>
+
         </div>
       </div>
 
@@ -99,8 +131,18 @@ IncludeTemplateLangFile(__FILE__);
       <div class="container py-1">
         <div class="row align-items-center">
           <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                    class="text-danger">.</span></strong></a></h1>
+            <!-- <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
+                    class="text-danger">.</span></strong></a></h1> -->
+                    <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/mylogo.php"
+	)
+);?>
           </div>
           <div class="col-4 col-md-4 col-lg-8">
             <nav class="site-navigation text-right text-md-right" role="navigation">
@@ -108,6 +150,23 @@ IncludeTemplateLangFile(__FILE__);
               <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
                   class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
 
+              <!--  Компонент меню - верхнее меню, просто разместить, рядом с текущей вёрсткой -->
+              <?$APPLICATION->IncludeComponent(
+	"bitrix:menu",
+	"",
+	Array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(""),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "N",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "N"
+	)
+);?>
               <ul class="site-menu js-clone-nav d-none d-lg-block">
                 <li class="active">
                   <a href="index.html">Home</a>
@@ -143,6 +202,66 @@ IncludeTemplateLangFile(__FILE__);
 
   <div class="slide-one-item home-slider owl-carousel">
 
+
+
+  <!-- компонент  ленты новостей, просто разместить пока -->
+  <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array("",""),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => $_REQUEST["ID"],
+		"IBLOCK_TYPE" => "ads",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array("","PRIORITY",""),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	)
+);?>
     <div class="site-blocks-cover" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/hero_bg_1.jpg);" data-aos="fade"
       data-stellar-background-ratio="0.5">
 
@@ -181,37 +300,88 @@ IncludeTemplateLangFile(__FILE__);
         <div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
           <div class="feature d-flex align-items-start">
             <span class="icon mr-3 flaticon-house"></span>
-            <div class="text">
+            <!-- <div class="text">
               <h2 class="mt-0">Wide Range of Properties</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.
               </p>
-            </div>
+            </div> -->
+  <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myinfofirst.php"
+	)
+);?>
           </div>
         </div>
         <div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
           <div class="feature d-flex align-items-start">
             <span class="icon mr-3 flaticon-rent"></span>
-            <div class="text">
+            <!-- <div class="text">
               <h2 class="mt-0">Rent or Sale</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.
               </p>
-            </div>
+            </div> -->
+            <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myinfosecond.php"
+	)
+);?>
           </div>
         </div>
         <div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
           <div class="feature d-flex align-items-start">
             <span class="icon mr-3 flaticon-location"></span>
-            <div class="text">
+            <!-- <div class="text">
               <h2 class="mt-0">Property Location</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.
               </p>
-            </div>
+            </div> -->
+            <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/myinfothird.php"
+	)
+);?>
           </div>
         </div>
       </div>
     </div>
   </div>
   <div class="site-section site-section-sm bg-light">
+
+  <!-- Компонент ленты новостей (bitrix:news.line) - ранее размещался уже, просто разместить, рядом с текущей вёрсткой -->
+  <?$APPLICATION->IncludeComponent(
+	"bitrix:news.line",
+	"",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"DETAIL_URL" => "",
+		"FIELD_CODE" => array("", ""),
+		"IBLOCKS" => array(),
+		"IBLOCK_TYPE" => "ads",
+		"NEWS_COUNT" => "9",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	)
+);?>
     <div class="container">
       <div class="row mb-5">
         <div class="col-12">
@@ -433,6 +603,30 @@ IncludeTemplateLangFile(__FILE__);
   </div>
 
   <div class="site-section">
+
+  <!-- Компонент ленты новостей (bitrix:news.line) - просто разместить,
+   рядом с текущей вёрсткой, внедрять ненужно, создать ИБ "Сервисы" -->
+   <?$APPLICATION->IncludeComponent(
+	"bitrix:news.line",
+	"",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"DETAIL_URL" => "",
+		"FIELD_CODE" => array("",""),
+		"IBLOCKS" => array(),
+		"IBLOCK_TYPE" => "service",
+		"NEWS_COUNT" => "20",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	)
+);?><br>
+   
+
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-md-7 text-center mb-5">
@@ -491,6 +685,30 @@ IncludeTemplateLangFile(__FILE__);
   </div>
 
   <div class="site-section bg-light">
+
+
+
+  <!-- Компонент ленты новостей (bitrix:news.line) - ранее размещался уже, просто разместить,
+   рядом с текущей вёрсткой, внедрять ненужно, настроить на ИБ "Новости" -->
+  <?$APPLICATION->IncludeComponent(
+	"bitrix:news.line",
+	"",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"DETAIL_URL" => "",
+		"FIELD_CODE" => array("", ""),
+		"IBLOCKS" => array(),
+		"IBLOCK_TYPE" => "news",
+		"NEWS_COUNT" => "3",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	)
+);?>
     <div class="container">
       <div class="row justify-content-center mb-5">
         <div class="col-md-7 text-center">
